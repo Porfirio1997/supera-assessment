7adb22536b5442e2396623e2963c6539c1c56521
Lucas-IFCE <lucasporfirio1997@gmail.com>
2023-07-17 12:37:10 -0300

    refactor segundo desafio.

--- desafio2/desafio2.java
+++ desafio2/desafio2.java
@@ -1,11 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Chalenge2 {
-    public static void main(String args[]) {
-        chalenge2(512.55);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double valor = Double.parseDouble(st.nextToken());
+        chalenge2(valor);
     }
 
     public static void chalenge2(Double valor){
         double[] Notas = {100,50,20,10,5,2};
         double[] Moedas= {1,0.5,0.25,0.10,0.05};
+
         System.out.println("NOTAS:");
         for(double Nota : Notas){
             int qtd = (int) (valor/Nota);
