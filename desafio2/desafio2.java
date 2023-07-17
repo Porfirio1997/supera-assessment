fca3fea2849c2cc420871012d38108475bf6596a
Lucas-IFCE <lucasporfirio1997@gmail.com>
2023-07-17 12:39:50 -0300

    Fix segundo desafio.

--- desafio2/desafio2.java
+++ desafio2/desafio2.java
@@ -14,7 +14,7 @@ public class Chalenge2 {
 
     public static void chalenge2(Double valor){
         double[] Notas = {100,50,20,10,5,2};
-        double[] Moedas= {1,0.5,0.25,0.10,0.05};
+        double[] Moedas= {1,0.5,0.25,0.10,0.05,0.01};
 
         System.out.println("NOTAS:");
         for(double Nota : Notas){
