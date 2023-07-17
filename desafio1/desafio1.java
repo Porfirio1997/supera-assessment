7b0c39468134130b7363645f6025e1cf408ab049
Lucas-IFCE <lucasporfirio1997@gmail.com>
2023-07-17 12:33:02 -0300

    refactor primeiro desafio.

--- desafio1/desafio1.java
+++ desafio1/desafio1.java
@@ -1,45 +1,31 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class Chalenge1 {
-    public static void main(String args[]) {
-        int[] intArray = new int[]{ 10,36,50,45,5,6,8,7,9,4,3,2,1 };
-        System.out.println(Arrays.toString(chalenge1(intArray)));
-    }
 
-    public static int[] chalenge1(int[] arr){
-        Arrays.sort(arr);
-        int[] Evens = FilterEvens(arr);
-        int[] Odds = FilterOdds(arr);
-        reverse(Odds);
-        return concatenateArrays(Evens,Odds);
-    }
 
-    public static int[] FilterOdds(int[] arr){
-        return Arrays
-                .stream(arr)
-                .filter(x -> x % 2 == 1)
-                .toArray();
-    }
+public class challenge1 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static int[] FilterEvens(int[] arr){
-        return Arrays
-                .stream(arr)
-                .filter(x -> x % 2 == 0)
-                .toArray();
-    }
-    public static void reverse(int[] array)
-    {
-        int n = array.length;
-        for (int i = 0; i < n / 2; i++) {
-            int temp = array[i];
-            array[i] = array[n - i - 1];
-            array[n - i - 1] = temp;
+        int lines = Integer.parseInt(st.nextToken());
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < lines; i++) {
+            st = new StringTokenizer(br.readLine());
+            numbers.add(Integer.parseInt(st.nextToken()));
         }
-    }
-    public static int[] concatenateArrays(int[] arr1,int[] arr2){
-        int[] result = new int[ arr1.length + arr2.length];
-        System.arraycopy(arr1, 0, result, 0, arr1.length);
-        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
-        return result;
+        var evens = numbers.stream().filter(l -> l % 2 == 0).sorted().collect(Collectors.toList());
+        var odds = numbers.stream().filter(l -> l % 2 != 0).sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        numbers = new ArrayList<>();
+        numbers.addAll(evens);
+        numbers.addAll(odds);
+        numbers.forEach(System.out::println);
     }
 }
