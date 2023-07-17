27f26b7f4ce78e113b4b92217d86d96dce5cff10
Lucas-IFCE <lucasporfirio1997@gmail.com>
2023-07-17 13:16:31 -0300

    refactor quarto desafio.

--- desafio4/desafio4.java
+++ desafio4/desafio4.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
 public class Chalenge4 {
 
     public static void main(String args[]) {
-        String Phrase = "I ENIL SIHTHSIREBBIG S";
-        System.out.println(FixOutput(Phrase));
+        Scanner leitor = new Scanner(System.in);
+        int n = leitor.nextInt();
+        for(int i=0; i < n ;i++){
+            String Phrase = readLine(leitor);
+            System.out.println(FixOutput(Phrase));
+        }
     }
 
     public static StringBuilder FixOutput(String Phrase){
         String FirstHalfPhrase = Phrase.substring(0, Phrase.length()/2);
         String LastHalfPhrase = Phrase.substring(Phrase.length()/2, Phrase.length());
         return new StringBuilder(FirstHalfPhrase).reverse().append( new StringBuilder(LastHalfPhrase).reverse());
+    }
+
+    public static String readLine(Scanner leitor) {
+        String line = leitor.nextLine();
+        while (line.isEmpty())
+            line = leitor.nextLine();
+        return line;
     }
 }
